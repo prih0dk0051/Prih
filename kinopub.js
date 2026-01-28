50c42a061a9681f60bffd07878614a804f5eab31
prih0dk0051 <dima.firsov.2011@gmail.com>
2026-01-28 18:59:48 +0300

    Add files via upload

--- kinopub.js
+++ kinopub.js
@@ -1,9 +1,17 @@
 (function () {
     'use strict';
 
-    function KinopubAPI(component, _object) {
+    // Основная логика плагина
+    function KinopubPlugin(component, _object) {
         var network = new Lampa.Reguest();
 
+        // Функция поиска (срабатывает при нажатии на кнопку в Источниках)
+        this.search = function(object, query) {
+            this.openAuth();
+            component.loading(false); // Убираем индикатор загрузки
+        };
+
+        // Окно авторизации
         this.openAuth = function() {
             var modal = $('<div><div class="broadcast__text" style="text-align:center;padding:20px;">Введите код на сайте <span style="color:#ffeb3b">kino.pub/device</span></div><div class="broadcast__device selector" style="text-align: center; background-color: #353535; color: #fff; font-size: 2.5em; padding: 15px; margin-top: 10px; border-radius: 10px;">ЗАГРУЗКА...</div></div>');
             
@@ -22,29 +30,28 @@
                     modal.find('.broadcast__device').text(found.code);
                 }
             }, function() {
-                modal.find('.broadcast__device').text('ОШИБКА API');
+                modal.find('.broadcast__device').text('ОШИБКА');
             });
-        };
-
-        this.search = function(object, query) {
-            this.openAuth();
-            component.loading(false);
         };
     }
 
+    // Инициализация плагина
     function startPlugin() {
-        Lampa.Component.add('kinopub_plugin', KinopubAPI);
+        // Регистрируем имя компонента
+        Lampa.Component.add('kp_plugin', KinopubPlugin);
 
-        // Добавляем кнопку именно в меню "Источник"
+        // КРИТИЧЕСКИЙ БЛОК: Добавление в меню "Источник"
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var sources = e.data.helper.sources || [];
-                var hasKP = sources.find(function(s){ return s.name == 'kinopub_plugin' });
                 
-                if(!hasKP) {
+                // Проверяем, нет ли уже такого источника
+                var hasKP = sources.find(function(s) { return s.name == 'kp_plugin' });
+                
+                if (!hasKP) {
                     sources.push({
                         title: 'Kinopub Код',
-                        name: 'kinopub_plugin',
+                        name: 'kp_plugin',
                         full_name: 'Kinopub',
                         icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14.5v-9l6 4.5-6 4.5z" fill="white"/></svg>'
                     });
@@ -53,17 +60,7 @@
         });
     }
 
+    // Запуск при готовности приложения
     if (window.appready) startPlugin();
     else Lampa.Events.on('app:ready', startPlugin);
 })();
-Lampa.Listener.follow('full', function (e) {
-    if (e.type == 'complite') {
-        var sources = e.data.helper.sources || [];
-        sources.push({
-            title: 'Kinopub Код',
-            name: 'kinopub_plugin',
-            full_name: 'Kinopub',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14.5v-9l6 4.5-6 4.5z" fill="white"/></svg>'
-        });
-    }
-});
