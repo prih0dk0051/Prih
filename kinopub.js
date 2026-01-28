4930eef37cb3e323ae665957fc927d5c3eff739f
prih0dk0051 <dima.firsov.2011@gmail.com>
2026-01-28 19:17:30 +0300

    Add files via upload

--- kinopub.js
+++ kinopub.js
@@ -1,19 +1,11 @@
 (function () {
     'use strict';
 
-    // Основная логика плагина
-    function KinopubPlugin(component, _object) {
-        var network = new Lampa.Reguest();
-
-        // Функция поиска (срабатывает при нажатии на кнопку в Источниках)
+    // 1. Создаем компонент Kinopub
+    function KinopubComponent(component, _object) {
         this.search = function(object, query) {
-            this.openAuth();
-            component.loading(false); // Убираем индикатор загрузки
-        };
-
-        // Окно авторизации
-        this.openAuth = function() {
-            var modal = $('<div><div class="broadcast__text" style="text-align:center;padding:20px;">Введите код на сайте <span style="color:#ffeb3b">kino.pub/device</span></div><div class="broadcast__device selector" style="text-align: center; background-color: #353535; color: #fff; font-size: 2.5em; padding: 15px; margin-top: 10px; border-radius: 10px;">ЗАГРУЗКА...</div></div>');
+            // Создаем окно с кодом
+            var modal = $('<div><div style="text-align:center;padding:20px;">Введите код на <b>kino.pub/device</b></div><div class="selector" style="text-align: center; background-color: #353535; color: #ffeb3b; font-size: 3em; padding: 10px; border-radius: 10px; border: 2px solid #fff;">A1B2C3</div></div>');
             
             Lampa.Modal.open({
                 title: 'Авторизация Kinopub',
@@ -23,35 +15,27 @@
                     Lampa.Controller.toggle('content');
                 }
             });
-
-            // Запрос кода (API Kinopub)
-            network.quiet('https://api.service.it/oauth2/device', function(found) {
-                if (found && found.code) {
-                    modal.find('.broadcast__device').text(found.code);
-                }
-            }, function() {
-                modal.find('.broadcast__device').text('ОШИБКА');
-            });
+            
+            // Убираем индикатор загрузки в Lampa
+            component.loading(false);
         };
     }
 
-    // Инициализация плагина
-    function startPlugin() {
-        // Регистрируем имя компонента
-        Lampa.Component.add('kp_plugin', KinopubPlugin);
+    // 2. Функция инициализации
+    function start() {
+        // Добавляем компонент в реестр Lampa
+        Lampa.Component.add('kinopub_source', KinopubComponent);
 
-        // КРИТИЧЕСКИЙ БЛОК: Добавление в меню "Источник"
+        // Внедряем кнопку в правую колонку "Источник"
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var sources = e.data.helper.sources || [];
                 
-                // Проверяем, нет ли уже такого источника
-                var hasKP = sources.find(function(s) { return s.name == 'kp_plugin' });
-                
-                if (!hasKP) {
+                // Проверяем, нет ли уже такого источника в списке
+                if (!sources.find(function(s) { return s.name == 'kinopub_source' })) {
                     sources.push({
                         title: 'Kinopub Код',
-                        name: 'kp_plugin',
+                        name: 'kinopub_source',
                         full_name: 'Kinopub',
                         icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14.5v-9l6 4.5-6 4.5z" fill="white"/></svg>'
                     });
@@ -60,7 +44,7 @@
         });
     }
 
-    // Запуск при готовности приложения
-    if (window.appready) startPlugin();
-    else Lampa.Events.on('app:ready', startPlugin);
+    // 3. Запуск при готовности приложения
+    if (window.appready) start();
+    else Lampa.Events.on('app:ready', start);
 })();
